697354b5695dc2d6908ea5909c30ad60747530bb
alecsey <topsecret@gamil.com>
2019-09-23 12:46:01 +0300

    implement: decode function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****':  ' '
 };
 
+const LETTER_LENGTH = 10;
+
+function alphabetEncoder(value) {
+    switch (value) {
+        case '11': return '-';
+        case '10': return '.';
+        case '**': return '*';
+    }
+    return '';
+}
+
 function decode(expr) {
-    // write your solution here
+    return [...expr].reduce(({decoded, stack, currentIndx}, value, indx) => {
+        if (indx % LETTER_LENGTH === 0) {
+            decoded.push([]);
+        }
+
+        if (stack.push(value) === 2) {
+            decoded[currentIndx].push(alphabetEncoder(stack.join('')));
+            stack.length = 0;
+        }
+
+        if (indx % LETTER_LENGTH === LETTER_LENGTH - 1) {
+            decoded[currentIndx] = MORSE_TABLE[decoded[currentIndx].join('')];
+            currentIndx += 1;
+        }
+
+        return {decoded, stack, currentIndx};
+    }, {
+        decoded: [],
+        stack: [],
+        currentIndx: 0
+    }).decoded.join('');
 }
 
 module.exports = {
